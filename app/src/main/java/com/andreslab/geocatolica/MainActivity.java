bdc95ab47f9b79f3dc06ec4fc5ccccdc7a55e7ff
Jaime Andrade <jaimeandrade454@gmail.com>
2019-07-31 06:22:41 -0500

    request GPS each 5 seconds

--- app/src/main/java/com/andreslab/geocatolica/MainActivity.java
+++ app/src/main/java/com/andreslab/geocatolica/MainActivity.java
@@ -32,14 +32,40 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+//-------------------------------------
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+/*import com.google.android.gms.location.LocationServices;
+
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.location.Location;*/
+import android.os.Build;
+/*import android.os.Bundle;
+import android.support.annotation.NonNull;*/
+import android.support.annotation.Nullable;
+/*import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;*/
+import android.widget.Toast;
+
+//-------------------------------------
+
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener, LocationListener{
 
     private static final int MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION = 1;
     Context context = this;
@@ -60,6 +86,24 @@ public class MainActivity extends AppCompatActivity {
     private String _namePlace;
 
     FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+
+    //---------------------------------
+
+    private Location location;
+    //private TextView locationTv;
+    private GoogleApiClient googleApiClient;
+    private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+    private LocationRequest locationRequest;
+    private static final long UPDATE_INTERVAL = 5000, FASTEST_INTERVAL = 5000; // = 5 seconds
+    // lists for permissions
+    private ArrayList<String> permissionsToRequest;
+    private ArrayList<String> permissionsRejected = new ArrayList<>();
+    private ArrayList<String> permissions = new ArrayList<>();
+    // integer for permissions results request
+    private static final int ALL_PERMISSIONS_RESULT = 1011;
+
+    //---------------------------------
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -128,39 +172,77 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+
+        //CATCH GPS TIMER
+        // we add permissions we need to request location of the users
+        permissions.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        permissions.add(Manifest.permission.ACCESS_COARSE_LOCATION);
+
+        permissionsToRequest = permissionsToRequest(permissions);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (permissionsToRequest.size() > 0) {
+                requestPermissions(permissionsToRequest.toArray(
+                        new String[permissionsToRequest.size()]), ALL_PERMISSIONS_RESULT);
+            }
+        }
+
+        // we build google api client
+        googleApiClient = new GoogleApiClient.Builder(this).
+                addApi(LocationServices.API).
+                addConnectionCallbacks(this).
+                addOnConnectionFailedListener(this).build();
+
+
+    }
+
+    private void resetParams(){
+        _latittude = 0.0;
+        _longitude = 0.0;
+        _namePlace = "";
+        btnSave.setEnabled(false);
+        btnSave.setAlpha(0.5f);
+        progressBar.setVisibility(View.GONE);
+    }
+
+    private void activeBtnSave(){
+        btnSave.setEnabled(true);
+        btnSave.setAlpha(1f);
     }
 
     private void savePlaceFirestore(Double lat, Double log, String namePlace){
         //ADD DATA FIRESTORE
-        // Create a new user with a first and last name
-        Map<String, Object> user = new HashMap<>();
-        user.put("name", namePlace);
-        user.put("latitude", lat);
-        user.put("longitude", log);
+
+        if (lat != 0.0 && log != 0.0 && lat !=null && log != null){
+            // Create a new user with a first and last name
+            Map<String, Object> user = new HashMap<>();
+            user.put("name", namePlace);
+            user.put("latitude", lat);
+            user.put("longitude", log);
 
 // Add a new document with a generated ID
-        db.collection("places")
-                .add(user)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                    @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Log.i(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
-                        _latittude = 0.0;
-                        _longitude = 0.0;
-                        _namePlace = "";
-                        progressBar.setVisibility(View.GONE);
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.i(TAG, "Error adding document", e);
-                        _latittude = 0.0;
-                        _longitude = 0.0;
-                        _namePlace = "";
-                        progressBar.setVisibility(View.GONE);
-                    }
-                });
+            db.collection("places")
+                    .add(user)
+                    .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                        @Override
+                        public void onSuccess(DocumentReference documentReference) {
+                            Log.i(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                            resetParams();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.i(TAG, "Error adding document", e);
+                            resetParams();
+                        }
+                    });
+        }else{
+            Toast toast1 = Toast.makeText(getApplicationContext(), "GPS ERROR...", Toast.LENGTH_SHORT);
+            //toast1.setGravity(Gravity.CENTER, , );
+            toast1.show();
+        }
+
     }
 
     private void readPlacesFirestore(){
@@ -198,7 +280,7 @@ public class MainActivity extends AppCompatActivity {
                 });
     }
 
-    private void fetchLocation() {
+    /*private void fetchLocation() {
 
 
         if (ContextCompat.checkSelfPermission(MainActivity.this,
@@ -266,7 +348,7 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
-    }
+    }*/
 
     private void showAlertToSaveName(){
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -283,25 +365,24 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 _namePlace = input.getText().toString();
-                fetchLocation();
+                //fetchLocation();
+                savePlaceFirestore(_latittude, _longitude, _namePlace);
+                user_location.setText("LAST PLACE : " + _namePlace.toUpperCase());
 
             }
         });
         builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                _latittude = 0.0;
-                _longitude = 0.0;
-                _namePlace = "";
+                resetParams();
                 dialog.cancel();
-                progressBar.setVisibility(View.GONE);
             }
         });
 
         builder.show();
     }
 
-    @Override
+    /*@Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         if(requestCode == MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION){
             if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
@@ -309,6 +390,172 @@ public class MainActivity extends AppCompatActivity {
             }else{
 
             }
+        }
+    }*/
+
+
+    // CATCH GPS TIMER
+
+    private ArrayList<String> permissionsToRequest(ArrayList<String> wantedPermissions) {
+        ArrayList<String> result = new ArrayList<>();
+
+        for (String perm : wantedPermissions) {
+            if (!hasPermission(perm)) {
+                result.add(perm);
+            }
+        }
+
+        return result;
+    }
+
+    private boolean hasPermission(String permission) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            return checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED;
+        }
+
+        return true;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (googleApiClient != null) {
+            googleApiClient.connect();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (!checkPlayServices()) {
+            user_location.setText("ERROR..");
+            //locationTv.setText("You need to install Google Play Services to use the App properly");
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // stop location updates
+        if (googleApiClient != null  &&  googleApiClient.isConnected()) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(googleApiClient, this);
+            googleApiClient.disconnect();
+        }
+    }
+
+    private boolean checkPlayServices() {
+        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+        int resultCode = apiAvailability.isGooglePlayServicesAvailable(this);
+
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (apiAvailability.isUserResolvableError(resultCode)) {
+                apiAvailability.getErrorDialog(this, resultCode, PLAY_SERVICES_RESOLUTION_REQUEST);
+            } else {
+                finish();
+            }
+
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                &&  ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+        // Permissions ok, we get last location
+        location = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+
+        if (location != null) {
+            _latittude = location.getLatitude();
+            _longitude = location.getLongitude();
+            activeBtnSave();
+            Log.i(TAG, "Latitude : " + location.getLatitude() + "\nLongitude : " + location.getLongitude());
+            //locationTv.setText("Latitude : " + location.getLatitude() + "\nLongitude : " + location.getLongitude());
+        }
+
+        startLocationUpdates();
+    }
+
+    private void startLocationUpdates() {
+        locationRequest = new LocationRequest();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(UPDATE_INTERVAL);
+        locationRequest.setFastestInterval(FASTEST_INTERVAL);
+
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                &&  ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "You need to enable permissions to display location !", Toast.LENGTH_SHORT).show();
+        }
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if (location != null) {
+            _latittude = location.getLatitude();
+            _longitude = location.getLongitude();
+            activeBtnSave();
+            Log.i(TAG, "Latitude : " + location.getLatitude() + "\nLongitude : " + location.getLongitude());
+            //locationTv.setText("Latitude : " + location.getLatitude() + "\nLongitude : " + location.getLongitude());
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch(requestCode) {
+            case ALL_PERMISSIONS_RESULT:
+                for (String perm : permissionsToRequest) {
+                    if (!hasPermission(perm)) {
+                        permissionsRejected.add(perm);
+                    }
+                }
+
+                if (permissionsRejected.size() > 0) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        if (shouldShowRequestPermissionRationale(permissionsRejected.get(0))) {
+                            new AlertDialog.Builder(MainActivity.this).
+                                    setMessage("These permissions are mandatory to get your location. You need to allow them.").
+                                    setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialogInterface, int i) {
+                                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                                requestPermissions(permissionsRejected.
+                                                        toArray(new String[permissionsRejected.size()]), ALL_PERMISSIONS_RESULT);
+                                            }
+                                        }
+                                    }).setNegativeButton("Cancel", null).create().show();
+
+                            return;
+                        }
+                    }
+                } else {
+                    if (googleApiClient != null) {
+                        googleApiClient.connect();
+                    }
+                }
+
+                break;
         }
     }
 }
